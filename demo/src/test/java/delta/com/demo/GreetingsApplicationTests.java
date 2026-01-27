3c5ccb91c1316ade86e945fd259ba577534035b8
brianacharya001 <62633019+brianacharya001@users.noreply.github.com>
2026-01-27 14:58:05 -0500

    Add files via upload
    init

---
+++ demo/src/test/java/delta/com/demo/GreetingsApplicationTests.java
@@ -0,0 +1,13 @@
+package delta.com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
